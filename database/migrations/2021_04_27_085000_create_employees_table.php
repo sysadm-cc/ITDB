5e65b0bfc5ea6a1c408c4021dd15bce9819b9c0d
sysadm-cc <sysadminjia@163.com>
2021-04-27 11:10:08 +0800

    ITDB

---
+++ database/migrations/2021_04_27_085000_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique()->comment('姓名');
+            $table->string('userid')->unique()->comment('用户ID');
+            $table->string('email')->unique()->nullable()->comment('电子邮件');
+            $table->string('department')->nullable()->comment('部门');
+            $table->boolean('gender')->nullable()->comment('性别');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
---
+++ database/seeds/EmployeesTableSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Item\Item_statustypes;
+
+class Item_statustypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		$nowtime = date("Y-m-d H:i:s",time());
+		
+		Item_statustypes::truncate();
+		
+        Item_statustypes::insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => '网管小贾',
+                'userid' => '20211202666',
+                'email' => 'wangguanxiaojia@sysadm.local',
+                'department' => 'IT',
+                'gender' => true,
+                'created_at' => $nowtime,
+                'updated_at' => $nowtime,
+                'deleted_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => '孔大力',
+                'userid' => '20211202888',
+                'email' => 'kongdali@sysadm.local',
+                'department' => '挨踢部',
+                'gender' => true,
+                'created_at' => $nowtime,
+                'updated_at' => $nowtime,
+                'deleted_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => '白小萌',
+                'userid' => '20211202999',
+                'email' => 'baixiaomeng@sysadm.local',
+                'department' => '财务部',
+                'gender' => false,
+                'created_at' => $nowtime,
+                'updated_at' => $nowtime,
+                'deleted_at' => NULL,
+            ),
+        ));
+	}
+}
